96e0628255e44bc64c87f3eaad6145f42fae44e0
JuanCruzRodriguez <rodriguezjuancruz02@hotmail.com>
2022-03-25 17:09:06 -0300

    subida de CartWidget y ItemListContainer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import NavBar from './components/NavBar';
+import ItemListContainer from './components/ItemListContainer';
 
-function App() {
+const App = () => {
   return (
     <>
     <NavBar></NavBar>
+    <ItemListContainer greeting="Hola mundo"></ItemListContainer>
     </>
   );
 }
